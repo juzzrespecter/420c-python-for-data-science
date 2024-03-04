948514a4a12da24b2daa4772226b5eeff862fb34
Daniel Rodriguez <daniel.rodriguezpueyo@telefonica.com>
2024-03-04 17:23:17 +0100

    starting/00

---
+++ starting/00/Hello.py
@@ -0,0 +1,28 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+new_values = ["World!", "France!", "Paris!", "42Paris"]
+
+# List substitution
+if "tata!" in ft_list:
+    ft_list[ft_list.index("tata!")] = new_values[0]
+
+# Tuple substitution
+if "toto!" in ft_tuple:
+    ft_tuple = tuple([new_values[1] if n == "toto!" else n for n in list(ft_tuple)])
+
+# Set substitution
+if "tutu!" in ft_set:
+    ft_set.remove("tutu!")
+    ft_set.add(new_values[2])
+
+# Dict substitution
+if "titi!" in ft_dict.values():
+    ft_dict["Hello"] = new_values[3]
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
