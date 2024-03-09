869d5dd2ab868393ed7894c23d916dabc6041c33
danrodri <danrodri@student.42madrid.com>
2024-03-09 18:14:44 +0100

    01/02

--- array/ex02/load_image.py
+++ array/ex02/load_image.py
@@ -1,13 +1,22 @@
+import numpy as np
 from numpy import ndarray as array
-from numpy import asarray
 from PIL import Image
 
-import matplotlib as plt
 
+def ft_load(path: str) -> array:
+    """
+    Loads an image, prints format and its pixels in RGB fmt.
+    Returns empty array in failure.
 
-def ft_load(path: str) -> array: #(you can return to the desired format)
+    - path: path to file to open.
+    """
     try:
-        img = asarray(Image.open(path))
-    except Exception as e:
-        raise e
-        pass
+        img = Image.open(path)
+        img_arr = np.asarray(img)
+        h, w, px_len = img_arr.shape
+        print(f"Image shape: height - {h}, width - {w}, px_size - {px_len}")
+        print(img_arr)
+        return img_arr
+    except IOError as e:
+        print(f"{type(e).__name__} :: {e}")
+    return []
